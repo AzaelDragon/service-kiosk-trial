309f5f0674d1a2f34b503b60103f71a58f24a433
David S. García (Azæl) <azael@dragonspark.tech>
2020-08-03 12:24:13 -0500

    fix: repair role guard mismatching

--- src/guards/role.guard.ts
+++ src/guards/role.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
@@ -10,9 +11,10 @@ export class RoleGuard implements CanActivate {
     if (!roles) {
       return true;
     }
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
-    return this.matchRoles(roles, user.roles);
+    const ctx = GqlExecutionContext.create(context);
+    const user = ctx.getContext().req.user;
+    console.log(user);
+    return this.matchRoles(roles, user.role);
   }
 
   matchRoles(roles: string[], userRole: string): boolean {
