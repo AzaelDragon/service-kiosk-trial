f2cb040276f73cc574d2236f2300f704abf8ef38
David S. García (Azæl) <azael@dragonspark.tech>
2020-08-03 15:29:54 -0500

    chore: enable swagger API integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Service Kiosk Rest API')
+    .setDescription('A simple API for managing service tickets.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
+  process.env.URL = await app.getUrl();
 }
 bootstrap();
